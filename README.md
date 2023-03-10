c3d5bc2ffec377905838df6aef71150841d362e9
chixPL <rkubsonpl@gmail.com>
2023-03-10 21:16:44 +0100

    Implementacja Masterminda

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# mastermind
-Mastermind board game implementation in Python with Player vs CPU mode and Knuth method algorithm.
+# Mastermind
+Implementacja gry planszowej Mastermind z trybem gry przeciwko komputerowi oraz algorytmem Knutha.
+
+## Zasady gry
+Jeden z graczy przyjmuje rolę kodera (code maker), a drugi rolę łamacza kodów lub Masterminda.
+Koder ma za zadanie ułożyć z 8 różnokolorowych kulek sekwencję 4 kulek, a następnie odpowiedzieć łamaczowi kodu określoną sekwencją kulek.
+Czarna kulka oznacza, że kulka o danym kolorze została umieszczona we właściwym miejscu.
+Biała kulka oznacza, że kolorowy kołek istnieje w kodzie, ale jest w złym miejscu.
+Celem jest określenie poprawnego kodu. Jeśli uda się to zrobić w ciągu określonej liczby prób, Mastermind wygrywa.
+
+Możesz zagrać w prostą implementację gry w HTML5 [tutaj.](https://webgamesonline.com/mastermind/)
+
+## Przegląd programu
+
+Program posiada dwa różne tryby: tryb gry i tryb algorytmu. Zasady dla obu są podane poniżej.
+
+**Zasady trybu gry**:
+- Kod do ustalenia składa się z 4 kulek.
+- 4 miejsca na kulki sprawdzające - czarne i białe
+- 10 prób
+- 8 kolorów (czerwony, zielony, niebieski, żółty, fioletowy, oliwkowy, różowy, morski).
+(info: w programie są oznaczone angielskimi skrótami: red, green, blue, yellow, violet, olive, pink, teal, white, blacK)
+- Dwa kolory kulek sprawdzających:
+    Czarny (oznaczony literą K) = kulka jest na właściwym miejscu
+    Biały (W) = kulka jest w kodzie, ale jest w złym miejscu
+- Gra przeciwko komputerowi
+
+**Zasady trybu algorytmu**:
+- Kod do ustalenia składa się z 4 kulek.
+- 4 miejsca na kulki sprawdzające - czarne i białe
+- Nieograniczona liczba prób
+- 6 kolorów, zapisanych w postaci liczb
+- Dwa kolory kulek sprawdzających:
+    Czarny (oznaczony literą K) = kulka jest na właściwym miejscu
+    Biały (W) = kulka jest w kodzie, ale jest w złym miejscu
+- Komputer gra przeciwko sobie, za cel mając osiągnięcie jak najmniejszej ilości tur do odgadnięcia kodu
+
+*Stworzone w Python 3.10.6 ze standardową biblioteką.*
+
+## Algorytm metody Knutha
+
+Można go przeczytać na Wikipedii [tutaj](https://en.wikipedia.org/wiki/Mastermind_(board_game)#Worst_case:_Five-guess_algorithm). Postaram się tutaj przedstawić lepsze wyjaśnienie.
+1. Zacznij od 1122 jako początkowej kombinacji.
+``python
+player_deck = [1, 1, 2, 2]
+```
+2. Wygeneruj wszystkie możliwe kombinacje poprzez iloczyn kartezjański wartości kolorów.
+``python 
+moves = list(itertools.product(colors, repeat=4))
+```
+3. Sprawdź wybraną kombinację z koderem, otrzymując jego odpowiedź składającą się z czarnych i białych kołków.
+4. Jeśli odpowiedź kodera to 4,0 (wszystkie czarne kołki), zakończ algorytm.
+5. W przeciwnym razie, sprawdź wszystkie możliwe ruchy względem odpowiedzi kodera w 4. Usuń wszystkie ruchy, które nie zwracają tej wartości.
+- Przykład:
+Poprawna kombinacja to 1234. Dla talii gracza=1122, otrzymujemy odpowiedź 1 czarny kołek i 1 biały kołek. Sprawdzamy wszystkie ruchy i usuwamy te, które nie zwracają 1 czarnego i 1 białego.
+6. Używamy algorytmu min max do wyboru następnego ruchu. Knuth stosuje konwencję wyboru ruchu o najmniejszej wartości liczbowej, np. 2345 jest mniejsze niż 3456. Z pozostałych ruchów wybierz ten, który ma minimalną sumę wartości liczbowych.
+7. Powtórz czynności od 3.
+
+### Podziękowania dla:
+- Donalda Knutha za wymyślenie algorytmu.
+- Ilana Schnella za bardziej przejrzyste wyjaśnienie algorytmu [(źródło)](https://github.com/ilanschnell/mastermind)
+
+[^1]: Źródło: http://www.cs.uni.edu/~wallingf/teaching/cs3530/resources/knuth-mastermind.pdf
--- mastermind.py
+++ mastermind.py
@@ -1,5 +1,10 @@
 """
-Gra Mastermind z komputerem
+Implementacja Mastermind w Pythonie
+Jakub Rutkowski (chixPL) 2023
+
+Po uruchomieniu programu użytkownik może wybrać jeden z dwóch możliwych trybów.
+"Tryb gry" jak najwierniej odwzorowuje grę planszową i jest to tryb, w którym ludzki gracz gra przeciwko komputerowi.
+
 Zasady:
 - 4 kolumny
 - 4 pola sprawdzające
@@ -7,6 +12,18 @@ Zasady:
 - 8 kolorów (red, green, blue, yellow, violet, olive, pink, teal)
 - 2 kolory sprawdzające (white, blacK)
 - gra z komputerem
+
+Tryb algorytmu używa uproszczonej wersji gry z zasadami zdefiniowanymi w oryginalnym artykule Knutha. Źródło jest dostępne w README.
+
+Zasady trybu algorytmicznego:
+- 4 kolumny
+- 4 pola sprawdzające (czarne i białe)
+- Nieograniczona liczba prób
+- 6 kolorów, uproszczonych jako liczby
+- 2 kolory sprawdzające (white, blacK)
+- Komputer gra przeciwko sobie za cel mając osiągnięcie jak najmniejszej ilości tur do odgadnięcia kodu
+
+Stworzone przy użyciu Pythona 3.10.6 i biblioteki standardowej.
 """
 import random
 import itertools
@@ -15,9 +32,10 @@ import time
 class Mastermind:
 
     def __init__(self) -> None:
-        self.c_deck = [] # talia komputera
-        self.gameEnd = False # koniec gry
-        self.tryb = ''
+        self.c_deck = [] # Talia komputera
+        self.gameEnd = False # Zmienna końca gry
+        self.turns = 0 # Licznik tur
+        self.tryb = '' #  Tryb gry lub algorytmu
         self.start_game()
     
     def start_game(self):
@@ -31,40 +49,51 @@ class Mastermind:
             print("a: Gracz vs Komputer")
             print("b: Algorytm (Komputer vs Komputer)")
             print("*" * 20)
-            self.tryb = input()
+            self.tryb = input() # Wybór trybu przez użytkownika (a=gra, b=algorytm)
             if self.tryb not in ['a','b']:
                 print("Źle wybrałeś tryb!")
             else:
                 break
 
         if self.tryb == 'a':
-            self.colours = ['R','G','B','Y','V','O','P','T']
-            self.create_deck()
+            self.colours = ['R','G','B','Y','V','O','P','T'] 
+            self.create_deck('game')
             while(self.gameEnd == False):
-                self.player_turn()
+                if(self.turns == 10):
+                    print("Przegrałeś.")
+                    exit()
+                else:
+                    self.player_turn()
         else:
             self.create_deck('cpu')
             self.algo()
 
-    def randomize(self, mode="real"):
+    def randomize(self, mode):
         deck = []
-        if mode == 'real':
-            elems = self.colours
+        if mode == 'game':
+            elems = self.colours #  Użyj tablicy kolorów zgodnie z zasadami gry planszowej
+            for i in range(4):
+                colour = random.choice(elems)
+                deck.append(colour)
+                elems.remove(colour)
+
+            return deck
         else:
             elems = []
-            for i in range(1, 6):
+            for i in range(1, 6): #  Użyj liczb dla lepszej przejrzystości matematycznej w trybie algorytmu
                 elems.append(i)
-        for i in range(4):
-            colour = random.choice(elems)
-            deck.append(colour)
-            elems.remove(colour)
-        return tuple(deck)
+            for i in range(4):
+                colour = random.choice(elems)
+                deck.append(colour)
+                elems.remove(colour)
+            return tuple(deck)
 
 
     def create_deck(self, mode):
         self.c_deck = self.randomize(mode)
+        print(self.c_deck)
 
-    def player_turn(self):
+    def player_turn(self): #  Pętla wprowadzania danych przez użytkownika
         while(True):
             guesses = input("Wybierz 4 kule oddzielone spacją: ")
             guesses = guesses.upper().split(' ')
@@ -76,6 +105,12 @@ class Mastermind:
 
     def check(self, input):
         self.answers = []
+        """
+        Sprawdzenie każdej kulki:
+        Jeśli kolor i indeks pasują, to jest to czarna kulka.
+        Jeśli tylko kolor pasuje (jest w tablicy c_deck, ale w złym miejscu), to jest to biała kulka.
+        Jeśli nie ma żadnej kulki o wybranym kolorze, zwróć pustego stringa. 
+        """
         if self.tryb == 'a':
             for i in range(0, len(input)):
                 if input[i] == self.c_deck[i]:
@@ -84,12 +119,16 @@ class Mastermind:
                     self.answers.append('W')
                 else:
                     self.answers.append('')
-            if(all(list(filter(lambda x: x=='K', self.answers)))):
+            if self.answers == ['K', 'K', 'K', 'K']: #  Jeśli wszystkie kołki w talii kontrolnej są czarne, zakończ grę
                 self.gameEnd = True
                 print("Wygrałeś!")
                 exit()
             else:
                 self.answers = sorted(self.answers, key=lambda x: (x == "", x.lower()))
+                """
+                ^^^ To używa manipulacji kodem ASCII do sortowania, ale żeby puste stringi były na końcu. 
+                Dzięki temu odpowiedź jest bardziej czytelna dla użytkownika końcowego.
+                """
                 print(self.answers)
         else:
             blacks = 0
@@ -99,10 +138,15 @@ class Mastermind:
                     blacks += 1
                 elif input[i] in self.c_deck:
                     whites += 1
+                # W trybie algorytmu puste kulki nie są liczone, ponieważ istnieją tylko dla wygody użytkowników.
             
-            return (blacks,whites)
+            return (blacks,whites) # Kolory są reprezentowane przez liczby dla lepszej matematycznej przejrzystości. 
 
     def check_all(self, moves, response):
+        """
+        Tylko w trybie algorytmu.
+        Sprawdza całą tablicę ruchów względem odpowiedzi kodera, aby wyeliminować niemożliwe ruchy.
+        """
         for i in moves:
             blacks = 0
             whites = 0
@@ -117,6 +161,11 @@ class Mastermind:
         return moves
     
     def score(self, moves):
+        """
+        Tylko w trybie algorytmu.
+        Knuth stosuje konwencję wyboru ruchu o najmniejszej wartości liczbowej, np. 2345 jest niższe niż 3456.
+        Dlatego zawsze wybierany jest ruch z najniższą sumą wartości.
+        """
         scores = []
         for i in moves:
             scores.append(sum(i))
@@ -127,28 +176,28 @@ class Mastermind:
         self.start_time = time.time()
         self.colours = [i for i in range(1, 7)]
         print("Jesteś w trybie Komputer vs Komputer")
-        self.c_deck = (self.randomize('cpu'))
+        self.c_deck = (self.randomize('cpu')) #  Stwórz talię komputerową w trybie uproszczonego algorytmu
         print("CPU: ",self.c_deck)
-        moves = list(itertools.product(self.colours, repeat=4))
-        self.p_deck = (1, 1, 2, 2)
+        moves = list(itertools.product(self.colours, repeat=4)) #  W znormalizowanej przez Knutha wersji gry możliwe jest 1296 (6^4) kodów.
+        self.p_deck = (1, 1, 2, 2) # Zaczynamy od 1, 1, 2, 2 zgodnie z zasadami metody Knutha.
 
         rounds = 0
 
         while(self.gameEnd != True):
             result = self.check(self.p_deck)
             rounds += 1
-            if(result != (4,0)):
+            if(result != (4,0)):  # Jeśli wszystkie kołki w talii kontrolnej są czarne, zakończ grę
                 moves = self.check_all(moves, result)
                 print("-----------------")
-                print(f"CPU: {self.c_deck}")
-                print(f"Moves: {moves}")
+                print(f"Komputer: {self.c_deck}")
+                print(f"Ruchy: {moves}")
                 scores = self.score(moves)
-                self.p_deck = moves[scores.index(min(scores))]
+                self.p_deck = moves[scores.index(min(scores))] #  Określa następny ruch, który ma być sprawdzony z koderem
                 moves.remove(self.p_deck)
-                print(f"Player Choice: {self.p_deck}") 
+                print(f"Wybór gracza: {self.p_deck}") 
             else:
                 print(f"Wygrałeś w {rounds} ruchach | Czas: {time.time() - self.start_time}")
                 self.gameEnd = True
                 exit()
 
-game = Mastermind()
+game = Mastermind() #  Rozpocznij grę
